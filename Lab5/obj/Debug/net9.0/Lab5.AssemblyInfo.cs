7567b3a8a402ef9dab4e466331c3af2df25fc8da
Bernardette Aragon <bsaragon.correo@url.edu.gt>
2026-08-07 09:52:29 -0600

    fix: agrega soluciones del Lab_5_Bernardette_Aragon

--- Lab5/obj/Debug/net9.0/Lab5.AssemblyInfo.cs
+++ Lab5/obj/Debug/net9.0/Lab5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b590d91674cb4da4b48e4c285d971a047024aae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
